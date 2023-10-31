3467dd347001e89dce77dc13c8ddcc05964e1b59
SatoshiIchimaru <ichimaru@hhq.suzuki.co.jp>
2023-10-31 03:10:20 +0000

    ISP-0000_create environment file_ichimaru,chiko

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>MODE: {import.meta.env.VITE_APP_TITLE}</p>
     </>
   )
 }
