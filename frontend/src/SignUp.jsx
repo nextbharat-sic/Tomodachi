3660da32eb5ab9ae397df35c2c8e8d6c1e1f1480
Yuta-Ishimura <ishimurayuta@hhq.suzuki.co.jp>
2023-11-15 12:45:06 +0000

    ISP-116_Created SignUp Page_(Suddhu,Dhanvi,Rino,Chiko)

---
+++ frontend/src/SignUp.jsx
@@ -0,0 +1,47 @@
+
+
+function SignUp(){
+    return(
+        <>
+        <h3>Sign Up</h3>
+        <div className="body">
+          <div className="item">
+             <label>User Name</label>
+          </div>
+           <div className="item">
+             <input type="text"></input>
+          </div>
+          <div className="item">
+             <label>Phone Number</label>
+          </div>
+           <div className="item">
+             <input type="text"></input>
+          </div>
+          <div className="item">
+             <label>Password</label>
+          </div>
+           <div className="item">
+             <input type="text"></input>
+          </div>
+          <div className="item">
+             <label>Confirm Password</label>
+          </div>
+           <div className="item">
+             <input type="text"></input>
+          </div>
+        <div className="item">
+             <input type="radio" />I Accept Terms Conditions  Privacy Policy of the Service
+          </div>
+        <button className="button">Sign UP</button>
+        
+        
+        </div>
+        </>
+        
+        )
+}
+
+
+
+
+export default SignUp
