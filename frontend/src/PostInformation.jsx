e86ae5fe623470b7a2a73ef3c8e8365925ce76c9
RnoSasamoto <sasamotor@hhq.suzui.co.jp>
2023-11-23 15:10:54 +0000

    Post Info Screen

---
+++ frontend/src/PostInformation.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const SignUp = () => {
+
+  return (
+    <>
+    
+      <h2 className="text-3xl font-bold underline">Sign Up</h2>
+    </>
+  );
+};
+
+export default SignUp;
