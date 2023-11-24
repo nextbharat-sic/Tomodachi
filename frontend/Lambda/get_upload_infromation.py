8ae2b9a68ac9d0b9b65987fe726048131a74c434
KhinNayChiKo <khinnaychiko@hhq.suzuki.co.jp>
2023-11-24 11:14:04 +0000

    ISP-81_create lambda function_Rino,Satoshi

---
+++ frontend/Lambda/get_upload_infromation.py
@@ -0,0 +1,54 @@
+import json
+import boto3
+import os
+
+def lambda_handler(event, context):
+
+    dynamodb_client = boto3.client('dynamodb')
+    post_informat_table_name = os.environ.get('POSTINFORMATIONTABLE')
+
+    query_params = {
+        'TableName': post_informat_table_name,
+        'IndexName': 'PTP-PCT-index',
+        'KeyConditionExpression': 'PTP = :post',
+        'ExpressionAttributeValues': {':post': {'S': 'post'}},
+        'ScanIndexForward': False,
+        }
+
+    response = dynamodb_client.query(**query_params)
+
+    post_list = []
+    for item in response['Items']:
+        post_date = item['PCT']['S'].split('.')[0]
+        pct = post_date.split('T')[0] + '/' + post_date.split('T')[1][:-3]
+        pmd_list = []
+        for pmd in item['PMD']['L']:
+            pmd_list.append(pmd['S'])
+
+        print(pmd_list)
+        post_list.append({
+            'PCT':pct,
+            'PMD':pmd_list,
+            'PJD':item['PJD']['S'],
+            'PTP':item['PTP']['S'],
+            'PID':item['PID']['S'],
+            'PMJ':item['PMJ']['S'],
+            'PDD':item['PDD']['S'],
+            'PST':item['PST']['BOOL'],
+            'PUID':item['PUID']['S'],
+            'PJT':item['PUT']['S'],
+            'PIT':item['PIT']['S'],
+        })
+
+    print(post_list)
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Headers" : "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
+        'body': json.dumps(post_list)
+    }
