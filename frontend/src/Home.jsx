cc065499d2bdc52f09988aed7f0ca6095f4582ce
RnoSasamoto <sasamotor@hhq.suzui.co.jp>
2023-11-24 10:30:00 +0000

    ISP-128_When click "share details", Move to Post screen or SignUp screen_Yuta,Chiko

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,4 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+
 const Home = () => {
+  const dispatch = useDispatch();
+  let pageStatus = useSelector((state) => state.pageStatus);
+  const signInState = useSelector((state) => state.isSignIn);
+
+  const movePostScreen = () => {
+    if (signInState) {
+      pageStatus = { type: "CHANGE_PAGE_STATE", payload: "PostJobPage" };
+    } else {
+      pageStatus = { type: "CHANGE_PAGE_STATE", payload: "SignUpPage" };
+    }
+    dispatch(pageStatus);
+  };
+
   return (
     <>
       <div style={{ border: "1px solid #333", margin: 3 }}>
@@ -6,7 +21,7 @@ const Home = () => {
       </div>
       <div style={{ border: "1px solid #333", margin: 3 }}>
         <div>Share career information</div>
-        <button>Share details</button>
+        <button onClick={movePostScreen}>Share details</button>
       </div>
       <p>recent post</p>
       <div style={{ border: "1px solid #333", margin: 3 }}>
