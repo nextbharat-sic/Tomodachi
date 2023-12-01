fe7e6253b9a214d9b387a1354ee74ab020e977d0
SatoshiIchimaru <ichimaru@hhq.suzuki.co.jp>
2023-12-01 06:20:59 +0000

    ISP-133_create home button and move home screen_Satoshi

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,8 +1,17 @@
+import { useDispatch } from "react-redux";
+
 const Header = () => {
+  const dispatch = useDispatch();
+  const moveHomeScreen = () => {
+    const pageStatus = { type: "CHANGE_PAGE_STATE", payload: "HomePage" };
+    dispatch(pageStatus);
+  };
+
   return (
     <>
       <div style={{ position: "absolute", top: 0, background: "blue" }}>
         Header
+        <button onClick={moveHomeScreen}>Home</button>
       </div>
     </>
   );
