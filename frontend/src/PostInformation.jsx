a778412ae9780cfd3b277893af3c7daf053987df
RnoSasamoto <sasamotor@hhq.suzui.co.jp>
2023-12-04 11:23:28 +0000

    ISP-83_Create_Post_Job_Info_Screen_Chiko,suddhu

--- frontend/src/PostInformation.jsx
+++ frontend/src/PostInformation.jsx
@@ -1,13 +1,83 @@
 import { useState } from "react";
 
-const SignUp = () => {
+const PostJob = () => {
+  const [file, setFile] = useState();
+
+  const showPhoto = (e) => {
+    setFile(URL.createObjectURL(e.target.files[0]));
+  };
 
   return (
-    <>
-    
-      <h2 className="text-3xl font-bold underline">Sign Up</h2>
-    </>
+    <div style={{ maxWidth: "75vw", margin: "auto" }}>
+      <form
+        style={{
+          padding: "7vw",
+          border: "1px solid #ccc",
+          borderRadius: "8px",
+        }}
+      >
+        <h2 style={{ textAlign: "center" }}>Information Details</h2>
+        <label htmlFor="infoTitle">Information Title</label>
+        <select
+          id="infoTitle"
+          name="infoTitle"
+          style={{ width: "61.5vw", marginBottom: "10px" }}
+        >
+          <option value="jobRelated">Job Related</option>
+        </select>
+        <label htmlFor="jobTitle">Job Title</label>
+        <input
+          type="text"
+          id="jobTitle"
+          name="jobTitle"
+          style={{ width: "61vw", marginBottom: "10px" }}
+          required
+        />
+        <label htmlFor="modeOfJob">Mode of Job</label>
+        <select
+          id="modeOfJob"
+          name="modeOfJob"
+          style={{ width: "61.5vw", marginBottom: "10px" }}
+        >
+          <option value="indoor">Indoor Work</option>
+          <option value="outdoor">Outdoor Work</option>
+        </select>
+        <label htmlFor="jobDescription">Job Description</label>
+        <textarea
+          id="jobDescription"
+          name="jobDescription"
+          rows="4"
+          style={{ width: "61vw", marginBottom: "10px" }}
+          required
+        ></textarea>
+        <label htmlFor="picture">Photos</label>
+
+        <input
+          type="file"
+          id="picture"
+          name="picture"
+          accept="image/*"
+          style={{ width: "61vw", marginBottom: "10px" }}
+          onChange={showPhoto}
+        />
+        <img src={file} style={{ width: "61vw", marginBottom: "10px" }} />
+        <button
+          type="submit"
+          style={{
+            backgroundColor: "#2F69F6",
+            color: "white",
+            padding: "10px 15px",
+            border: "none",
+            borderRadius: "4px",
+            cursor: "pointer",
+            width: "20vw",
+          }}
+        >
+          Upload
+        </button>
+      </form>
+    </div>
   );
 };
 
-export default SignUp;
+export default PostJob;
