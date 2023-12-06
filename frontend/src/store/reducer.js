40d2ec204850d43970b5676cc4f40a7a43916d34
SatoshiIchimaru <ichimaru@hhq.suzuki.co.jp>
2023-12-06 07:14:38 +0000

    ISP-140_Get_UserID_and_store Rino Sailesh

--- frontend/src/store/reducer.js
+++ frontend/src/store/reducer.js
@@ -1,6 +1,7 @@
 const initialState = {
   isSignIn: false,
   pageStatus: "HomePage",
+  userID: "",
 };
 
 const rootReducer = (state = initialState, action) => {
@@ -10,6 +11,9 @@ const rootReducer = (state = initialState, action) => {
 
     case "CHANGE_PAGE_STATE":
       return { ...state, pageStatus: action.payload };
+
+    case "SET_USER_ID":
+      return { ...state, userID: action.payload };
 
     default:
       return state;
