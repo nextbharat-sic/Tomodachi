17517a9d93ca2201a54c977a8ee99c5a8f0c9adf
SatoshiIchimaru <ichimaru@hhq.suzuki.co.jp>
2023-12-06 07:21:26 +0000

    ISP-140 remove console statemnt Rino, Sailesh

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -33,7 +33,6 @@ const SignUp = () => {
     };
 
     const result = await postUserInformation(userInformation);
-    console.log(result);
     if (result.status == "Success") {
       setIsLoading(false);
       alert("User Registration is completed!");
