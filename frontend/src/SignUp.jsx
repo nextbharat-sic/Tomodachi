bbb0c117d0955150e574336e453710b8a0318d71
RnoSasamoto <sasamotor@hhq.suzui.co.jp>
2023-12-13 11:06:38 +0000

    ISP-92_Display Privacy Policy in Sign Up Screen_ Yuta, Suddhu

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -115,6 +115,35 @@ const SignUp = () => {
             ></input>
           </div>
         </Box>
+        <div style={{ marginLeft: "8vw", marginTop: "2vh" }}>
+          <div
+            style={{
+              margin: "4px",
+              padding: "4px",
+              width: "80vw",
+              height: "40vh",
+              overflowX: "hidden",
+              overflowY: "auto",
+              textAlign: "justify",
+            }}
+          >
+            <h4 style={{ textAlign: "center" }}>Terms and Condition</h4>
+            It is a good platform to learn programming. It is an educational
+            website. Prepare for the Recruitment drive of product based
+            companies like Microsoft, Amazon, Adobe etc with a free online
+            placement preparation course. The course focuses on various MCQ's &
+            Coding question likely to be asked in the interviews & make your
+            upcoming placement season efficient and successful. Also, any geeks
+            can help other geeks by writing articles on the GeeksforGeeks,
+            publishing articles follow few steps that are Articles that need
+            little modification /improvement from reviewers are published first.
+            To quickly get your articles reviewed, please refer existing
+            articles, their formatting style, coding style, and try to make you
+            are close to them. In case you are a beginner, you may refer
+            Guidelines to write an Article
+          </div>
+        </div>
+
         <div style={{ margin: "10px" }}>
           <div style={{ display: "flex", alignItems: "center" }}>
             <div>
