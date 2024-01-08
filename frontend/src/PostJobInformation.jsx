4c248c54377f91f789f91b33f2c9d2889d8c33d2
SatoshiIchimaru <ichimaru@hhq.suzuki.co.jp>
2024-01-08 11:14:51 +0000

    ISP-186_add new category and separate component_ ichimaru, rino

--- frontend/src/PostJobInformation.jsx
+++ frontend/src/PostJobInformation.jsx
@@ -1,276 +1,43 @@
 import { useState, useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
-// import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import postJobInformation from "./clients/postjobinformation.js";
+import { useSelector } from "react-redux";
+import JobMarket from "./JobMarket.jsx";
 import Box from "@mui/material/Grid";
 
 const PostJobInformation = () => {
-  // const [imageFile, setImageFile] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const userID = useSelector((state) => state.userID);
-  const accountName = useSelector((state) => state.accountName);
-  const phoneNumber = useSelector((state) => state.phoneNumber);
-  // const inputImageFile = useRef();
-  const dispatch = useDispatch();
-  let date = new Date();
-  let localDate =
-    date.getFullYear() +
-    "-" +
-    (date.getMonth() + 1).toString().padStart(2, "0") +
-    "-" +
-    date.getDate().toString().padStart(2, "0");
-  // let localTime = date.toLocaleTimeString();
+  const [category, setCategory] = useState("");
 
-  const [jobData, setJobData] = useState({
-    userId: userID,
-    informationTitle: "jobRelated",
-    jobTitle: "",
-    deadlineDate: "",
-    modeOfJob: "indoor",
-    jobDescription: "",
-    image: "",
-    accountName: accountName,
-    phoneNumber: phoneNumber,
-  });
-
-  const homePageStatus = () => {
-    const storePage = { type: "CHANGE_PAGE_STATE", payload: "HomePage" };
-    dispatch(storePage);
+  const categoryDataChange = (event) => {
+    const value = event.target.value;
+    setCategory(value);
   };
-
-  // const showImage = (event) => {
-  //   setImageFile(URL.createObjectURL(event.target.files[0]));
-  //   setJobData({
-  //     ...jobData,
-  //     image: event.target.files[0],
-  //   });
-  // };
-
-  const handleInputDataChange = (event) => {
-    const { name, value } = event.target;
-    setJobData({
-      ...jobData,
-      [name]: value,
-    });
-  };
-
-  const confirmUpload = (event) => {
-    event.preventDefault();
-    const deadlineValidation = checkDeadlineDate();
-    if (!deadlineValidation) {
-      alert("Deadline date has passed");
-    } else {
-      const isConfirm = confirm("Are you sure you want to upload?");
-      if (isConfirm) {
-        setIsLoading(true);
-        uploadJobInfo();
-        // checkImageFile();
-      }
-    }
-  };
-
-  // const checkImageFile = () => {
-  //   const File = inputImageFile.current.files[0];
-  //   if (File && File.size > 0) {
-  //     uploadFile();
-  //   } else {
-  //     uploadJobInfo();
-  //   }
-  // };
-
-  const checkDeadlineDate = () => {
-    if (jobData.deadlineDate >= localDate) {
-      return true;
-    }
-    return false;
-  };
-
-  const uploadJobInfo = async () => {
-    try {
-      const jobInformation = {
-        userId: jobData.userId,
-        informationTitle: jobData.informationTitle,
-        jobTitle: jobData.jobTitle,
-        deadlineDate: jobData.deadlineDate,
-        modeOfJob: jobData.modeOfJob,
-        jobDescription: jobData.jobDescription,
-        image: jobData.image,
-        accountName: jobData.accountName,
-        phoneNumber: jobData.phoneNumber,
-      };
-
-      const response = await postJobInformation(jobInformation);
-
-      if (response.status === "Success") {
-        setIsLoading(false);
-        homePageStatus();
-        alert("Upload information is completed!");
-      } else {
-        setIsLoading(false);
-        alert("Upload information is failed!");
-      }
-    } catch (error) {
-      setIsLoading(false);
-      console.error("Error uploading information:", error);
-    }
-  };
-
-  // const uploadFile = async () => {
-  //   const uploadImageFile = inputImageFile.current.files[0];
-  //   const uploadFileName =
-  //     userID + "_" + localDate + "_" + localTime + "_" + uploadImageFile.name;
-  //   jobData.image = uploadFileName;
-
-  //   const s3Client = new S3Client({
-  //     region: import.meta.env.VITE_APP_S3_REGION,
-  //     credentials: {
-  //       accessKeyId: import.meta.env.VITE_APP_S3_ACCESS_KEY,
-  //       secretAccessKey: import.meta.env.VITE_APP_S3_SECRET_ACCESS_KEY,
-  //     },
-  //   });
-
-  //   const S3Params = {
-  //     Bucket: import.meta.env.VITE_APP_S3_BUCKET_NAME,
-  //     Key: uploadFileName,
-  //     Body: uploadImageFile,
-  //   };
-
-  //   try {
-  //     const result = await s3Client.send(new PutObjectCommand(S3Params));
-  //     uploadJobInfo();
-  //   } catch (err) {
-  //     console.log("Error", err);
-  //   }
-  // };
 
   return (
     <>
       <div>
-        <form onSubmit={confirmUpload}>
-          <h2 style={{ textAlign: "center" }}>Information Details</h2>
+        <h2 style={{ textAlign: "center" }}>Information Details</h2>
 
-          <div style={{ textAlign: "left", paddingLeft: "3vw" }}>
-            <label>Information Title</label>
-          </div>
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <select
-              name="informationTitle"
-              value={jobData.informationTitle}
-              onChange={handleInputDataChange}
-              style={{
-                width: "86vw",
-                margin: "10px",
-                height: "5vh",
-                borderRadius: "10px",
-              }}
-            >
-              <option value="jobRelated">Job Related</option>
-            </select>
-          </Box>
-
-          <div style={{ textAlign: "left", paddingLeft: "3vw" }}>
-            <label>Job Title</label>
-          </div>
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <input
-              type="text"
-              name="jobTitle"
-              placeholder="Write job title"
-              value={jobData.jobTitle}
-              onChange={handleInputDataChange}
-              style={{
-                width: "86vw",
-                margin: "10px",
-                height: "5vh",
-                borderRadius: "10px",
-                borderWidth: "1px",
-              }}
-              required
-            />
-          </Box>
-
-          <div style={{ textAlign: "left", paddingLeft: "3vw" }}>
-            <label>Deadline Date</label>
-          </div>
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <input
-              type="date"
-              name="deadlineDate"
-              value={jobData.deadlineDate}
-              onChange={handleInputDataChange}
-              style={{
-                width: "86vw",
-                margin: "10px",
-                height: "5vh",
-                borderRadius: "10px",
-                borderWidth: "1px",
-              }}
-              required
-            />
-          </Box>
-
-          <div style={{ textAlign: "left", paddingLeft: "3vw" }}>
-            <label>Mode of Job</label>
-          </div>
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <select
-              name="modeOfJob"
-              value={jobData.modeOfJob}
-              onChange={handleInputDataChange}
-              style={{
-                width: "86vw",
-                margin: "10px",
-                height: "5vh",
-                borderRadius: "10px",
-              }}
-            >
-              <option value="indoor">Indoor Work</option>
-              <option value="outdoor">Outdoor Work</option>
-            </select>
-          </Box>
-
-          <div style={{ textAlign: "left", paddingLeft: "3vw" }}>
-            <label>Job Description</label>
-          </div>
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <textarea
-              rows="4"
-              name="jobDescription"
-              placeholder="Write description of the job"
-              value={jobData.jobDescription}
-              onChange={handleInputDataChange}
-              style={{
-                width: "86vw",
-                margin: "10px",
-                borderRadius: "10px",
-              }}
-              required
-            ></textarea>
-          </Box>
-          {/* Photo upload function is not included in MVP1
-          <label>Photos</label>
-          <input
-            type="file"
-            ref={inputImageFile}
-            accept="image/*"
-            style={{ width: "61vw", marginBottom: "10px" }}
-            onChange={showImage}
-          />
-          <img src={imageFile} style={{ width: "61vw", marginBottom: "10px" }} />
-          */}
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <button
-              type="submit"
-              disabled={isLoading}
-              style={{
-                backgroundColor: "#2F69F6",
-                color: "#e0f2f1",
-              }}
-            >
-              {isLoading ? "Upload now..." : "Upload"}
-            </button>
-          </Box>
-        </form>
+        <div style={{ textAlign: "left", paddingLeft: "3vw" }}>
+          <label>Category</label>
+        </div>
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <select
+            name="category"
+            value={category}
+            onChange={categoryDataChange}
+            style={{
+              width: "86vw",
+              margin: "10px",
+              height: "5vh",
+              borderRadius: "10px",
+            }}
+          >
+            <option value="">select category</option>
+            <option value="jobRelated">Job Related</option>
+            <option value="careerRelatedNews">Career Related News</option>
+            <option value="ThandaTalks">Thanda Talks</option>
+          </select>
+        </Box>
+        {category == "jobRelated" ? <JobMarket /> : ""}
       </div>
     </>
   );
