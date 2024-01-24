e12085d20d62e0a7909b4a4494cadb0c0d7f66ab
RnoSasamoto <sasamotor@hhq.suzui.co.jp>
2024-01-24 12:15:46 +0000

    ISP-200_Edit const variable name_chiko

--- frontend/src/clients/generatesignupotpcode.js
+++ frontend/src/clients/generatesignupotpcode.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 
 export default async function generateSignUpOtpCode(userInformation) {
-  const postGenerateOTPURL = import.meta.env.VITE_APP_GENERATE_SIGN_UP_OTP_CODE;
+  const postGenerateSignUpOTPURL = import.meta.env
+    .VITE_APP_GENERATE_SIGN_UP_OTP_CODE;
   const header = { "Content-Type": "application/json" };
   try {
-    const response = await axios.post(postGenerateOTPURL, userInformation, {
-      headers: header,
-    });
+    const response = await axios.post(
+      postGenerateSignUpOTPURL,
+      userInformation,
+      {
+        headers: header,
+      },
+    );
     return response.data;
   } catch (error) {
     return error;
