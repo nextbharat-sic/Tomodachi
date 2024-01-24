a0ad4352dd4194b67532259608c5a1232cc6f87c
SatoshiIchimaru <ichimaru@hhq.suzuki.co.jp>
2024-01-24 10:18:40 +0000

    ISP-231_change image file to png_Ichimaru

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect, Fragment } from "react";
 // import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import Box from "@mui/material/Grid";
 import getUploadInformation from "./clients/getuploadinformation.js";
-import homeImage from "./assets/home_img.jpeg";
+import homeImage from "./assets/home_img.png";
 import { useTranslation } from "react-i18next";
 
 const Home = () => {
@@ -217,10 +217,10 @@ const Home = () => {
               {jobData.PIT === "thandaTalks"
                 ? "Thanda Talks"
                 : jobData.PIT === "jobMarket"
-                ? "Job Market"
-                : jobData.PIT === "careerRelatedNews"
-                ? "Career Related News"
-                : ""}
+                  ? "Job Market"
+                  : jobData.PIT === "careerRelatedNews"
+                    ? "Career Related News"
+                    : ""}
             </span>
             {jobData.PIT === "thandaTalks" ? (
               <span
