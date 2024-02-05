11503590e1583f1f4b0dca1947d349ceb41e410e
SatoshiIchimaru <ichimaru@hhq.suzuki.co.jp>
2024-02-05 11:32:04 +0000

    ISP-253_force number keyboard test_Yuta

--- frontend/src/CheckOtp.jsx
+++ frontend/src/CheckOtp.jsx
@@ -126,6 +126,11 @@ const CheckOtp = () => {
         }}
         TextFieldsProps={{
           inputProps: {
+            //ISP-253
+            type: "tel",
+            pattern: "\\d*",
+            inputMode: "numeric",
+            //ISP-253
             style: {
               color: isDarkMode ? "white" : "black",
               padding: "1vh 0",
