b54be00137c5fa16b09c01616cab0e85378a84d2
RnoSasamoto <sasamotor@hhq.suzui.co.jp>
2024-02-07 12:43:56 +0000

    ISP-262_Edit_change-deadline-date_lambda_function_chiko

--- frontend/Lambda/change_deadline_date.py
+++ frontend/Lambda/change_deadline_date.py
@@ -6,11 +6,8 @@ from datetime import datetime, timedelta
 def convert_utc_to_india(utc_datetime):
     ist_offset = timedelta(hours=5, minutes=30)
     india_datetime = utc_datetime + ist_offset
-
     india_date = india_datetime.strftime('%Y-%m-%d')
-    india_time = india_datetime.strftime('%H:%M:%S')
-
-    return india_date, india_time
+    return india_date
 
 def lambda_handler(event, context):
 
@@ -22,64 +19,96 @@ def lambda_handler(event, context):
     else:
         body = event
     
-    utc_time = datetime.utcnow()
-    current_date, current_time = convert_utc_to_india(utc_time)
+    # Get 'PUID' attribute value from DB
+    response_Post_Data = dynamodb_client.get_item(
+        TableName = postinformation_table_name,
+        Key = {'PID': {'S': body['postId']}, 'PIT': {'S': body['informationTitle']}},
+    )
+    post_item = response_Post_Data.get('Item')
     
-    # Parse deadline date
-    parsed_deadline_date = datetime.strptime(body['deadlineDate'], '%Y-%m-%d')
-    deadline_date = parsed_deadline_date.strftime('%Y-%m-%d')
+    if post_item:
+        post_User_Id = post_item.get('PUID', {}).get('S')
+        print("Post User Id: ", post_User_Id)
+        
+        # Check post-recruiter
+        if body['postUserId'] != post_User_Id:
+            print('User is not post-recruiter')
+            return {
+                'statusCode': 500,
+                'headers': {
+                    'Content-Type': 'application/json',
+                    "Access-Control-Allow-Headers" : "*",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                },
+                'body': json.dumps({"status": "Failed"})
+            }
+        else:
+            print('User is post-recruiter')
             
-    # Check Status ActiveToClose
-    if body['statusTo'] == 'Close':
-        if deadline_date >= current_date:
-            print('ActiveToCloseError')
-            return {
-                'statusCode': 500,
-                'headers': {
-                    'Content-Type': 'application/json',
-                    "Access-Control-Allow-Headers" : "*",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-                },
-                'body': json.dumps({
-                    "status": "Failed",
-                })
-            }
-    # Status CloseToActive
+            utc_time = datetime.utcnow()
+            current_date_india = convert_utc_to_india(utc_time)
+    
+            # Parse deadline date
+            parsed_deadline_date = datetime.strptime(body['deadlineDate'], '%Y-%m-%d')
+            deadline_date = parsed_deadline_date.strftime('%Y-%m-%d')
+            
+            print("deadline_date:" + deadline_date)
+            print("current_date_india:" + current_date_india)
+            # Check deadline date
+            if deadline_date >= current_date_india:
+                print('Deadline date must be smaller than current date')
+                return {
+                    'statusCode': 500,
+                    'headers': {
+                        'Content-Type': 'application/json',
+                        "Access-Control-Allow-Headers" : "*",
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                    'body': json.dumps({"status": "Failed"})
+                }
+            else:
+                # Change deadline_date value in DB
+                try:
+                    dynamodb_client.update_item(
+                        TableName = postinformation_table_name,
+                        Key = {'PID': {'S': body['postId']}, 'PIT': {'S': body['informationTitle']}},
+                        UpdateExpression = "SET PDD = :newDeadlineDate",
+                        ExpressionAttributeValues = {":newDeadlineDate": {'S': deadline_date}},
+                    )
+            
+                    return {
+                        'statusCode': 200,
+                        'headers': {
+                            'Content-Type': 'application/json',
+                            "Access-Control-Allow-Headers" : "*",
+                            "Access-Control-Allow-Origin": "*",
+                            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                        },
+                        'body': json.dumps({"status": "Success"})
+                    }
+                except Exception as e:
+                    print(f"Update failed: {e}")
+                    return {
+                        'statusCode': 500,
+                        'headers': {
+                            'Content-Type': 'application/json',
+                            "Access-Control-Allow-Headers" : "*",
+                            "Access-Control-Allow-Origin": "*",
+                            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                        },
+                        'body': json.dumps({"status": "Failed"})
+                    }
     else:
-        if deadline_date < current_date:
-            print('CloseToActiveError')
-            return {
-                'statusCode': 500,
-                'headers': {
-                    'Content-Type': 'application/json',
-                    "Access-Control-Allow-Headers" : "*",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-                },
-                'body': json.dumps({
-                    "status": "Failed",
-                })
-            }
- 
- 
-    try:
-        dynamodb_client.update_item(
-            TableName = postinformation_table_name,
-            Key = {'PID': {'S': body['postId']}, 'PIT': {'S': body['informationTitle']}},
-            UpdateExpression = "SET PDD = :newDeadlineDate",
-            ExpressionAttributeValues = {":newDeadlineDate": {'S': deadline_date}},
-        )
-
+        print("PUID not found")
         return {
-            'statusCode': 200,
-            'body': json.dumps({
-                "status": "Success",
-            })
-        }
-    except Exception as e:
-        print(f"Update failed: {e}")
-        return {
-            'statusCode': 500,
-            'body': json.dumps({"status": "Failed"})
-        }
+                    'statusCode': 500,
+                    'headers': {
+                        'Content-Type': 'application/json',
+                        "Access-Control-Allow-Headers" : "*",
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                    'body': json.dumps({"status": "Failed"})
+                }
