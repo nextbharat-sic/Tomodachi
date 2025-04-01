872a45bceafa6b80ad8a798411f520f368ff7907
Yuta-Ishimura <147363036+Yuta-Ishimura@users.noreply.github.com>
2025-04-01 12:42:54 +0900

    Update Header.jsx

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,3 +1,4 @@
+// Copyright © 2025 Suzuki Motor Corporation All Rights Reserved
 import { useDispatch } from "react-redux";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
