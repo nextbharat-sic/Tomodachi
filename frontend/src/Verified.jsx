2d90cf2acc41731a439b37ce00bdcfe062b022f9
Yuta-Ishimura <147363036+Yuta-Ishimura@users.noreply.github.com>
2025-04-01 14:37:42 +0900

    Update Verified.jsx

--- frontend/src/Verified.jsx
+++ frontend/src/Verified.jsx
@@ -1,3 +1,4 @@
+// Copyright © 2025 Suzuki Motor Corporation All Rights Reserved
 import { useDispatch, useSelector } from "react-redux";
 import Box from "@mui/material/Grid";
 import TaskAltIcon from "@mui/icons-material/TaskAlt";
