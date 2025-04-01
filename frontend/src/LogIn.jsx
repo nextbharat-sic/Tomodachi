019e8123f3627fff879294e220fbadab2c2bebe7
Yuta-Ishimura <147363036+Yuta-Ishimura@users.noreply.github.com>
2025-04-01 14:36:14 +0900

    Update LogIn.jsx

--- frontend/src/LogIn.jsx
+++ frontend/src/LogIn.jsx
@@ -1,3 +1,4 @@
+// Copyright © 2025 Suzuki Motor Corporation All Rights Reserved
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import Link from "@mui/material/Link";
