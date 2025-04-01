518d3a8a64274a565fdcc5eaf6aab4c4ad53bc5f
Yuta-Ishimura <147363036+Yuta-Ishimura@users.noreply.github.com>
2025-04-01 14:24:14 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// Copyright © 2025 Suzuki Motor Corporation All Rights Reserved
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import Box from "@mui/material/Grid";
